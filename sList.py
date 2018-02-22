12a19113e05d630e4f2db0050c43d2385882825b
AberDerBart <nonatz@web.de>
2018-02-23 00:31:50 +0100

    switch to sync endpoint (breaks sending to server)

--- sList.py
+++ sList.py
@@ -6,56 +6,56 @@ class List:
 	def __init__(self,server,listId):
 		self.listId=listId
 		self.server=server
+		self.items=[]
+		self.title=''
 		self.sync()
 	def sync(self):
-		r=requests.get(self.listUrl())
+		r=requests.get(self.syncUrl())
 		if(r.status_code == 200):
 			data=json.loads(r.text)
 
 			self.title=data.get('title','')
 			self.title=data.get('title','')
-			self.items=[]
-
-			for item in data.get('items',[]):
-				self.items.append(Item(item))
-	def listUrl(self):
-		return '{}/api/{}'.format(self.server,self.listId)
+			self.items=data.get('items','')
+	def syncUrl(self):
+		return '{}/api/{}/sync'.format(self.server,self.listId)
 	def show(self):
 		if self.items:
 			print(self.title)
 		else:
 			print(self.title,'(empty)')
 		for item in self.items:
-			print("- {}".format(str(item)))
+			print("- {}".format(itemStr(item)))
 	def add(self,value):
-		r=requests.post(self.listUrl()+'/items?parse',json=value)
+		self.items.append({'stringRepresentation':value})
 		self.sync()
 	def delete(self,item):
-		r=requests.delete(self.listUrl()+'/items/'+item.id())
+		self.items.remove(item)
 		self.sync()
 	def edit(self,item,value):
-		r=requests.put(self.listUrl()+'/items/{}?parse'.format(item.id()),json=value)
+		if('id' in item):
+			itemId=item['id']
+			item.clear()
+			item['id']=itemId
+		else:
+			item.clear()
+		item['stringRepresentation']=value
 		self.sync()
 	def clear(self):
 		for item in self.items:
 			self.delete(item)
 		self.sync()
 
-class Item:
-	def __init__(self,itemDict):
-		self.itemDict=itemDict;
-	def id(self):
-		return self.itemDict.get('id')
-	def name(self):
-		return self.itemDict.get('name')
-	def __str__(self):
-		string=self.itemDict.get('name')
-		amount=self.itemDict.get('amount',None)
-		if amount:
-			unit=amount.get('unit',None)
-			value=amount.get('value',None)
-			if unit:
-				string='{} {}'.format(unit,string)
-			if value:
-				string='{} {}'.format(value,string)
-		return string
+def itemStr(itemDict):
+	if 'stringRepresentation' in itemDict:
+		return itemDict['stringRepresentation']
+	string=itemDict.get('name')
+	amount=itemDict.get('amount',None)
+	if amount:
+		unit=amount.get('unit',None)
+		value=amount.get('value',None)
+		if unit:
+			string='{} {}'.format(unit,string)
+		if value:
+			string='{} {}'.format(value,string)
+	return string
