291e3e8c1fd681e1d6897a9f0dec01f778656dd7
AberDerBart <nonatz@web.de>
2018-02-22 17:09:11 +0100

    Revert "improve config parsing"
    This reverts commit f42bace7fd4689e428e3f0dc09d2a2e40368cea0.
    
    The intention was to enfoce the config file to be in a config folder in order to store the cache files there as well (for a future offline feature).
    However, a more elegant way is to simply specify the location of the cache files in a config parameter

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
 import argparse
 import rcfile
 
-
 DEFAULT_SERVER = 'https://list.tilman.ninja'
-DEFAULT_LIST = 'Demo'
+DEFAULT_LIST   = 'Demo'
 
-# setup cli parser
 parser=argparse.ArgumentParser()
 subParsers=parser.add_subparsers(dest="command")
 subParsers.required=False
@@ -27,23 +25,4 @@ parser.add_argument("--server",help="the URL of the server to contact")
 
 args=parser.parse_args()
 
-# config from cli args 
-argsDict=args.__dict__
-
-# config from evironment
-envDict=rcfile.get_environment('shoppingList')
-
-# config from config file
-configDict=rcfile.get_config('shoppingList','shoppingList','~/.shoppingList/config')
-
-
-# merge config with priority cli > environment > config file
-config={}
-for conf in [configDict,envDict,argsDict]:
-	config=rcfile.merge(conf,config)
-
-# set default values if none set
-if not config.get('server'):
-	config['server'] = DEFAULT_SERVER
-if not config.get('list'):
-	config['list'] = DEFAULT_LIST
+config=rcfile.rcfile('shoppingList',args.__dict__)
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import actions
 from sList import List
 from config import config,args
 
-sl=List(config.get('server'),config.get('list'))
+
+sl=List(config.get('server') or DEFAULT_SERVER,config.get('list') or DEFAULT_LIST)
 
 if(args.command):
 	actions.actionDict[args.command](sl,args)
