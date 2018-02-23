87b9947bdf23937731fdcdcf50e6b6d7af2af4cf
AberDerBart <nonatz@web.de>
2018-02-23 01:39:29 +0100

    implement writing and reading of cache file

--- cache.py
+++ cache.py
@@ -3,8 +3,23 @@ import os.path
 import json
 from sList import * 
 
-class CachedList(List):
-	def __init__(self,server,listId,cacheFile):
-		super().__init__(server,listId)
+class Cache:
+	def __init__(self,listInstance):
+		self.listInstance=listInstance	
+		self.cacheFileName=os.path.expanduser(config.get('cachedir')+'/'+self.listInstance.listId+'.json')
+		self.cacheFileDir= os.path.dirname(self.cacheFileName)
+	def write(self,data):
+		if not os.path.exists(self.cacheFileDir):
+			os.makedirs(self.cacheFileDir)
+		f=open(self.cacheFileName,'w')
+		json.dump(data,f)
+		f.close()
+	def read(self):
+		f=open(self.cacheFileName)
+		if f:
+			data=json.load(f)
+			f.close()
+			return data
+		else:
+			return None
 
-		self.cacheFile=cacheFile
