05ef6a261e0127a51f2afbcce81d6972956702eb
AberDerBart <nonatz@web.de>
2018-04-09 20:07:32 +0200

    implement category caching

--- category.py
+++ category.py
@@ -1,26 +1,29 @@
 import requests
-
-class Category:
-	def __init__(self,catDict):
-		self.id=catDict.get('id')
-		self.name=catDict.get('name')
-		self.shortName=catDict.get('shortName')
-		self.color=catDict.get('color')
-		self.lightText=catDict.get('lightText')
+from cache import Cache
+from config import config
 
 class CategoryList:
 	def __init__(self,server,listId):
 		self.server=server
 		self.listId=listId
+		self.categories=None
+		if 'cachedir' in config:
+			self.cache=Cache(self.server,self.listId,'.cache.json')
+			cacheData=self.cache.read()
+			if cacheData:
+				self.categories=cacheData
+		else:
+			self.cache=None
 	def pull(self):
 		r=requests.get('{}/api/{}/categories'.format(self.server,self.listId))
 		if(r.status_code == 200):
-			print(r.text)
 			data=r.json()
 			self.categories={}
 
 			for cat in data:
 				catId=cat.get('id')
 				if(catId):
-					self.categories[catId]=Category(cat)
+					self.categories[catId]=cat
+			if self.cache:
+				self.cache.write(self.categories)
 	
